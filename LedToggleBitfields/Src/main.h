a08fb86d08ac0790e25496da61ab4c15e62512cd
Korhan Mutludoğan <korhanmd@gmail.com>
2023-04-03 10:32:56 +0200

    Implement a struct for GPIOx_ODR register

--- LedToggleBitfields/Src/main.h
+++ LedToggleBitfields/Src/main.h
@@ -38,4 +38,24 @@ typedef struct {
 	uint32_t otghsulpi_en	:1;
 }RCC_AHB1ENR_t;
 
+typedef struct {
+	uint32_t pin_0		:1;
+	uint32_t pin_1		:1;
+	uint32_t pin_2		:1;
+	uint32_t pin_3		:1;
+	uint32_t pin_4		:1;
+	uint32_t pin_5		:1;
+	uint32_t pin_6		:1;
+	uint32_t pin_7		:1;
+	uint32_t pin_8		:1;
+	uint32_t pin_9		:1;
+	uint32_t pin_10		:1;
+	uint32_t pin_11		:1;
+	uint32_t pin_12		:1;
+	uint32_t pin_13		:1;
+	uint32_t pin_14		:1;
+	uint32_t pin_15		:1;
+	uint32_t reserved	:16;
+}GPIOx_ODR_t;
+
 #endif /* MAIN_H_ */
