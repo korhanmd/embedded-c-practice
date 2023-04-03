482fd592c7efa3728ad049570c6ddf56e2c63d8c
Korhan Mutludoğan <korhanmd@gmail.com>
2023-04-03 10:29:25 +0200

    Implement a struct for RCC_AHB1ENR register

---
+++ LedToggleBitfields/Src/main.h
@@ -0,0 +1,41 @@
+/*
+ * main.h
+ *
+ *  Created on: Apr 3, 2023
+ *      Author: korhan
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdint.h>
+
+typedef struct {
+	uint32_t gpioa_en		:1;
+	uint32_t gpiob_en		:1;
+	uint32_t gpioc_en		:1;
+	uint32_t gpiod_en		:1;
+	uint32_t gpioe_en		:1;
+	uint32_t gpiof_en		:1;
+	uint32_t gpiog_en		:1;
+	uint32_t gpioh_en		:1;
+	uint32_t gpioi_en		:1;
+	uint32_t reserved1		:3;
+	uint32_t crc_en			:1;
+	uint32_t reserved2		:3;
+	uint32_t reserved3		:2;
+	uint32_t bkpsram_en		:1;
+	uint32_t reserved4		:1;
+	uint32_t ccmdat_en		:1;
+	uint32_t dma1_en		:1;
+	uint32_t dma2_en		:1;
+	uint32_t reserved5		:2;
+	uint32_t ethmac_en		:1;
+	uint32_t ethmactx_en	:1;
+	uint32_t ethmacrx_en	:1;
+	uint32_t ethmacptp_en	:1;
+	uint32_t otghs_en		:1;
+	uint32_t otghsulpi_en	:1;
+}RCC_AHB1ENR_t;
+
+#endif /* MAIN_H_ */
